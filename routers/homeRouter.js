9e97bcb8cda5ca02e5ecdbbfd60be38a4154a88f
shankarjatin <shankarjatin1005@gmail.com>
2022-11-12 13:37:59 -0800

    login page made

--- routers/homeRouter.js
+++ routers/homeRouter.js
@@ -20,12 +20,21 @@ try{
        })
        userData.save( err=>{
         if(err){
-            console.log(err)
+            console.log("err")
         }else{
             res.render("register",{title:"Done",password:"",email:""})
 
         }
        })
+
+const useremail =await HomeSchema.findOne({email:email})
+if(email===useremail.email){
+    res.render("register",{title:"",password:"",email:"USer exists"})
+}
+else{
+    console.log("error")
+}
+
     }
     else{
         res.render("register",{title:"",password:"password not matching",email:""})
@@ -36,6 +45,23 @@ try{
     
 
 }});
+
+
+Router.post("/login", (req,res)=>{
+    const {
+        email,
+        password
+    }= req.body;
+
+    HomeSchema.findOne({email:email},(err,result)=>{
+       if(email===result.email && password===result.password){
+        res.render("user",{ name: result.uname})
+       }
+       else{
+        console.log(err);
+       }
+    })
+})
 
 module.exports = Router;
 
